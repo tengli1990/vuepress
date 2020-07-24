a98c58a9ff0ab49fa3755c01ba8bbdc8122e157a
李腾 <liteng@dankegongyu.com>
2020-07-24 18:25:06 +0800

    新增每日一题答案解析

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Knowledge is infinite',
+  title: '学无止境',
   description: '我的个人网站',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
